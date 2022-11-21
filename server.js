82cd09d43010b10574524797565cda5434a7deb8
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-21 23:08:26 +0000

    Listening for event in the pong server

--- server.js
+++ server.js
@@ -11,6 +11,16 @@ const PORT = 3000
 server.listen(PORT)
 console.log(`listening on ${PORT}...`)
 
+let readyPlayerCount = 0
+
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id)
+
+    socket.on('ready', () => {
+        console.log('Player ready', socket.id)
+        readyPlayerCount += 1
+        if (readyPlayerCount === 2) {
+            // broadcast('startGame')
+        }
+    })
 })
