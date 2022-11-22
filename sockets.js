913cae28815466b6357e277299848b4760a84ac4
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-22 15:37:16 +0000

    Implementing rooms

--- sockets.js
+++ sockets.js
@@ -1,32 +1,35 @@
 let readyPlayerCount = 0
 
-
 function listen(io) {
     const pongNamespace = io.of('/pong')
     pongNamespace.on('connection', (socket) => {
         console.log('a user connected', socket.id)
+        let room
 
         socket.on('ready', () => {
+            room = 'room' + Math.floor(readyPlayerCount / 2)
+            socket.join(room)
             console.log('Player ready', socket.id)
             readyPlayerCount += 1
             if (readyPlayerCount % 2 === 0) {
                 // broadcast('startGame') to every client connected
-                pongNamespace.emit('startGame', socket.id)
+                pongNamespace.in(room).emit('startGame', socket.id)
             }
         })
 
         // listen to the 'paddleMove' event and broadcast the 'paddleMove' event
         // to all clients except the one that originally send it.
         socket.on('paddleMove', (paddleData) => {
-            socket.broadcast.emit('paddleMove', paddleData)
+            socket.to(room).emit('paddleMove', paddleData)
         })
 
         socket.on('ballMove', (ballData) => {
-            socket.broadcast.emit('ballMove', ballData)
+            socket.to(room).emit('ballMove', ballData)
         })
 
         socket.on('disconnect', (reason) => {
             console.log(`Client ${socket.id} disconnected: ${reason}`)
+            socket.leave(room)
         })
     })
 }
