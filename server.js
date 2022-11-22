4ba5867b3c280295702710d9059be991a5ffeb7c
OpenTech-Consult <opentech.consult@gmail.com>
2022-11-22 12:50:40 +0000

    Using socket with Express

--- server.js
+++ server.js
@@ -1,41 +1,18 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server, {
+const http = require('http')
+const io = require('socket.io')
+
+const apiServer = require('./api')
+const sockets = require('./sockets')
+const PORT = 3000
+
+const httpServer = http.createServer(apiServer)
+const socketServer = io(httpServer, {
     cors: {
         origin: '*',
         method: ['GET', 'POST']
     }
 })
 
-const PORT = 3000
-
-server.listen(PORT)
+httpServer.listen(PORT)
 console.log(`listening on ${PORT}...`)
-
-let readyPlayerCount = 0
-
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id)
-
-    socket.on('ready', () => {
-        console.log('Player ready', socket.id)
-        readyPlayerCount += 1
-        if (readyPlayerCount % 2 === 0) {
-            // broadcast('startGame') to every client connected
-            io.emit('startGame', socket.id)
-        }
-    })
-
-    // listen to the 'paddleMove' event and broadcast the 'paddleMove' event
-    // to all clients except the one that originally send it.
-    socket.on('paddleMove', (paddleData) => {
-        socket.broadcast.emit('paddleMove', paddleData)
-    })
-
-    socket.on('ballMove', (ballData) => {
-        socket.broadcast.emit('ballMove', ballData)
-    })
-
-    socket.on('disconnect', (reason) => {
-        console.log(`Client ${socket.id} disconnected: ${reason}`)
-    })
-})
+sockets.listen(socketServer)
---
+++ sockets.js
@@ -0,0 +1,37 @@
+let readyPlayerCount = 0
+
+
+function listen(io) {
+    io.on('connection', (socket) => {
+        console.log('a user connected', socket.id)
+
+        socket.on('ready', () => {
+            console.log('Player ready', socket.id)
+            readyPlayerCount += 1
+            if (readyPlayerCount % 2 === 0) {
+                // broadcast('startGame') to every client connected
+                io.emit('startGame', socket.id)
+            }
+        })
+
+        // listen to the 'paddleMove' event and broadcast the 'paddleMove' event
+        // to all clients except the one that originally send it.
+        socket.on('paddleMove', (paddleData) => {
+            socket.broadcast.emit('paddleMove', paddleData)
+        })
+
+        socket.on('ballMove', (ballData) => {
+            socket.broadcast.emit('ballMove', ballData)
+        })
+
+        socket.on('disconnect', (reason) => {
+            console.log(`Client ${socket.id} disconnected: ${reason}`)
+        })
+    })
+}
+
+module.exports = {
+    listen,
+}
+
+
